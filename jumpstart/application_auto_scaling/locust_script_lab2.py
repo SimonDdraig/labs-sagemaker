df635e3b7d17ea1a01298ccd0bf8a6fe262c92e8
Simon Davies <simon.ddraig@gmail.com>
2025-09-26 17:46:41 +0800

    jumpstart-lab1 allowed img2txt prompts

--- jumpstart/application_auto_scaling/locust_script_lab2.py
+++ jumpstart/application_auto_scaling/locust_script_lab2.py
@@ -2,6 +2,7 @@ import json
 import logging
 import os
 import time
+import random
 
 import boto3
 from botocore.config import Config
@@ -14,21 +15,49 @@ region = os.environ["AWS_REGION"]
 contentType = os.environ["CONTENT_TYPE"]
 payload = os.environ["PAYLOAD"]
 endpointName = os.environ['ENDPOINT_NAME']
+modelType = os.environ['MODEL_TYPE']
  
-
 class BotoClient:
     def __init__(self, host):
-        # Consider removing retry logic to get accurate picture of failure in locust
-        config = Config(
-            region_name=region, retries={"max_attempts": 0, "mode": "standard"}
-        )
+        if modelType == "llm":
+            config = Config(
+                region_name=region, retries={"max_attempts": 0, "mode": "standard"}
+            )
+        else:
+            # increase timeout for image models
+            config = Config(
+                region_name=region,
+                retries={"max_attempts": 0, "mode": "standard"},
+                read_timeout=60,  # 1 minute
+                connect_timeout=60
+            )
 
         self.sagemaker_client = boto3.client("sagemaker-runtime", config=config)
         self.endpoint_name = endpointName
         self.content_type = contentType
-        self.payload = payload
+
+        if modelType == "llm":
+            self.payload = payload
+        else:
+            # for image models we need to create a json payload with additional parameters
+            # may need to change these per image model - default is good for stable diffusion
+            payload_dict = {
+                "text_prompts": [
+                    {"text": payload}
+                ],
+                "width": 128,
+                "height": 128,
+                "cfg_scale": 7.0,
+                "steps": 50,
+                "seed": random.randint(0, 4294967295)
+            }
+            self.payload = json.dumps(payload_dict).encode("utf-8")
 
     def send(self):
+        from PIL import Image
+        import base64
+        from io import BytesIO
+
         request_meta = {
             "request_type": "InvokeEndpoint",
             "name": "SageMaker",
@@ -46,7 +75,17 @@ class BotoClient:
                 Body=self.payload,
                 ContentType=self.content_type,
             )
-            logging.info(response["Body"].read())
+            if modelType == "llm":
+                logging.info(response["Body"].read())
+            else:
+                logging.info("Image generated")
+                result = json.loads(response["Body"].read())
+                image_b64 = result["generated_images"][0]
+                image_bytes = base64.b64decode(image_b64)
+                image = Image.open(BytesIO(image_bytes))
+                image.show()
+                image.close()
+
         except Exception as e:
             request_meta["exception"] = e
 
